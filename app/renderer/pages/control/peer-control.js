55375464076f3fb225f39598f69f48f5e16473a9
zhouxlin <xlin173@foxmail.com>
2020-09-04 12:10:47 +0800

    fix bug

--- app/renderer/pages/control/peer-control.js
+++ app/renderer/pages/control/peer-control.js
@@ -1,10 +1,6 @@
 const EventEmitter = require('events')
 const peer = new EventEmitter()
-const{desktopCapturer, ipcRenderer} = require('electron')
-const ipc = require('../../../main/ipc')
-const control = require('../../../main/window/control')
-const { error } = require('console')
-
+const{ipcRenderer} = require('electron')
 
 // getScreenStream()
 // peer.on('robot', function (type, data) {
@@ -41,11 +37,12 @@ dc.onerror = (error) => {
 
 // onicecandidate iceEvent
 // addIceCandidate
-pc.onicecandidate = function (e) {
-    console.log('candidate', JSON.stringify(e.candidate))
-    if (e.candidate) {
-        ipcRenderer.send('forward', 'control-candidate', e.candidate.toJSON())
-    }
+pc.onicecandidate = function (e) { //
+    console.log('control receive a candidate', JSON.stringify(e.candidate))
+    pc.addIceCandidate(e.candidate)
+    // if (e.candidate) {
+    //     ipcRenderer.send('forward', 'control-candidate', e.candidate.toJSON())
+    // }
 }
 
 ipcRenderer.on('candidate', (e, candidate) => {
@@ -72,7 +69,7 @@ async function createOffer(){
         offerToReceiveVideo: true
     })
     await pc.setLocalDescription(offer)
-    console.log('offer json:', JSON.stringify(offer))
+    console.log('control create offer success：', JSON.stringify(offer))
     return pc.localDescription
 }
 
@@ -86,10 +83,12 @@ async function setRemote(answer) {
 
 ipcRenderer.on('answer', (e, answer) => {
     setRemote(answer)
+    let iceCandidate = new RTCIceCandidate(answer)
+    pc.addIceCandidate(iceCandidate)
+    // ipcRenderer.send('forward', 'puppet-candidate', iceCandidate.toJSON())
 })
 
 // window.setRemote = setRemote
-
 // 监听addtracks
 pc.ontrack = function (e) {
     if (e.streams && e.streams[0]) {
